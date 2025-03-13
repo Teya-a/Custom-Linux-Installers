d97fc419a29cc722e49055dbfac9aaba60f300dc
Teya <#@>
2025-03-13 05:45:56 -0400

    #

---
+++ Arch/Dependencies.py
@@ -0,0 +1,9 @@
+import os
+import sys
+import re
+import signal
+import subprocess
+import getpass
+from art import banner
+
+__all__ = ["os", "sys", "re", "signal", "subprocess", "getpass", "banner"]
